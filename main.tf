44ecae84b7d332024fb86f20a413ec60350d1e7d
gopivurata <113236420+gopivurata@users.noreply.github.com>
2023-06-06 13:36:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -67,7 +67,7 @@ module "eks" {
 
       min_size     = 1
       max_size     = 3
-      desired_size = 2
+      desired_size = 1
     }
 
     two = {
@@ -76,7 +76,7 @@ module "eks" {
       instance_types = ["t3.small"]
 
       min_size     = 1
-      max_size     = 2
+      max_size     = 1
       desired_size = 1
     }
   }
